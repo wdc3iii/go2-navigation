a876c176d7427014620d888bf038590874c4008b
Will Compton <wdc3iii@gmail.com>
2025-02-02 22:58:22 -0800

    refacted DTMPC node

--- go2_dyn_tube_mpc/go2_dyn_tube_mpc/dyn_tube_mpc_node.py
+++ go2_dyn_tube_mpc/go2_dyn_tube_mpc/dyn_tube_mpc_node.py
@@ -6,6 +6,8 @@ import numpy as np
 from go2_dyn_tube_mpc_msg.msg import Trajectory
 from obelisk_estimator_msgs.msg import EstimatedState
 from obelisk_control_msgs.msg import VelocityCommand
+from nav_msgs.msg import OccupancyGrid, OccupancyGridUpdate
+from sensor_msgs.msg import LaserScan
 from rclpy.lifecycle import LifecycleState, TransitionCallbackReturn
 
 from obelisk_py.core.control import ObeliskController
@@ -13,15 +15,8 @@ from obelisk_py.core.obelisk_typing import ObeliskControlMsg, is_in_bound
 
 import casadi as ca
 import torch
-from go2_dyn_tube_mpc.casadi_utils import (
-    quadratic_objective, dynamics_constraints, initial_condition_equality_constraint, setup_parameterized_ocp
-)
+from go2_dyn_tube_mpc.dynamic_tube_mpc import DynamicTubeMPC
 
-import debugpy
-
-debugpy.listen(("localhost", 5678))  # Port 5678 for debugging
-print("Waiting for debugger...")
-debugpy.wait_for_client()  # Pauses execution until debugger is attached
 
 class DynamicTubeMPCNode(ObeliskController):
     """Dynamic Tube MPC.
@@ -61,17 +56,19 @@ class DynamicTubeMPCNode(ObeliskController):
         assert np.all(self.v_max_bound > 0) and np.all(self.v_min_bound <= 0)
         # Cost matrices
         self.declare_parameter("Q", [1., 0., 0., 0., 1., 0., 0., 0., 0.1])          # State cost
-        self.Q = np.array(self.get_parameter("Q").get_parameter_value().double_array_value).reshape(self.n, self.n)
+        Q = np.array(self.get_parameter("Q").get_parameter_value().double_array_value).reshape(self.n, self.n)
         self.declare_parameter("Qf", [10., 0., 0., 0., 10., 0., 0., 0., 10.])       # Terminal Cost
-        self.Qf = np.array(self.get_parameter("Qf").get_parameter_value().double_array_value).reshape(self.n, self.n)
+        Qf = np.array(self.get_parameter("Qf").get_parameter_value().double_array_value).reshape(self.n, self.n)
         self.declare_parameter("R", [0.1, 0., 0., 0., 0.1, 0., 0., 0., 0.1])        # Temporal weights on state cost
-        self.R = np.array(self.get_parameter("R").get_parameter_value().double_array_value).reshape(self.m, self.m)
+        R = np.array(self.get_parameter("R").get_parameter_value().double_array_value).reshape(self.m, self.m)
         self.declare_parameter("Q_schedule", np.linspace(0, 1, self.N).tolist())    # Schedule on state cost (allow more deviation of trajectory from plan near beginning)
-        self.Q_sched = np.array(self.get_parameter("Q_schedule").get_parameter_value().double_array_value)
+        Q_sched = np.array(self.get_parameter("Q_schedule").get_parameter_value().double_array_value)
         self.declare_parameter("Rv_first", [0., 0., 0., 0., 0., 0., 0., 0., 0.])      # First order rate penalty on input
-        self.Rv_first = np.array(self.get_parameter("Rv_first").get_parameter_value().double_array_value).reshape(self.n, self.n)
+        Rv_first = np.array(self.get_parameter("Rv_first").get_parameter_value().double_array_value).reshape(self.n, self.n)
         self.declare_parameter("Rv_second", [0., 0., 0., 0., 0., 0., 0., 0., 0.])     # Second order rate penalty on input
-        self.Rv_second = np.array(self.get_parameter("Rv_second").get_parameter_value().double_array_value).reshape(self.n, self.n)
+        Rv_second = np.array(self.get_parameter("Rv_second").get_parameter_value().double_array_value).reshape(self.n, self.n)
+
+        self.dtmpc = DynamicTubeMPC(self.N, self.n, self.m, Q, Qf, R, Rv_first, Rv_second, Q_sched)
 
         # Declare subscriber to velocity commands
         self.register_obk_subscription(
@@ -86,13 +83,22 @@ class DynamicTubeMPCNode(ObeliskController):
             key="sub_vel_lim_key",  # key can be specified here or in the config file
             msg_type=VelocityCommand
         )
+        self.register_obk_subscription(
+            "sub_map_setting",
+            self.map_update_callback,  # type: ignore
+            key="sub_map_key",  # key can be specified here or in the config file
+            msg_type=OccupancyGridUpdate
+        )
+        self.register_obk_subscription(
+            "sub_scan_setting",
+            self.laser_scan_callback,  # type: ignore
+            key="sub_scan_key",  # key can be specified here or in the config file
+            msg_type=LaserScan
+        )
 
     def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
         """Configure the controller."""
-        # TODO: DEBUGGIN
-        # super().on_configure(state)
-        self.n = 3
-        self.m = 3
+        super().on_configure(state)
         self.px_z = np.zeros((3,))
         self.px_v = np.zeros((3,))
         self.z_ref = np.zeros((self.n, self.N + 1))
@@ -103,9 +109,7 @@ class DynamicTubeMPCNode(ObeliskController):
 
         self.recieved_path = False
 
-        setup_parameterized_ocp(
-            self.N, self.n, self.m, self.Q, self.R, self.Qf, self.Q_sched, self.Rv_first, self.Rv_second
-        )
+        # TODO: Set the initial map for the DTMPC
         
         return TransitionCallbackReturn.SUCCESS
 
@@ -134,16 +138,32 @@ class DynamicTubeMPCNode(ObeliskController):
         else:
             self.get_logger().error(f"Estimated State base velocity size does not match URDF! Size is {len(x_hat_msg.v_base)} instead of 6.")
 
+        self.dtmpc.set_initial_condition(self.px_z)
+
     def plan_callback(self, plan_msg: Trajectory):
         # Accepts a trajectory which is a sequence of waypoints
         self.plan = np.array(plan_msg.z).reshape(plan_msg.horizon + 1, plan_msg.n)
         self.recieved_path = True
+
+        # TODO: use this to set reference for DTMPC. Maybe with smoothing?
+
+        # TODO: reset DTMPC warmstart
+
+    def map_update_callback(self, map_update_msg: OccupancyGridUpdate):
+        # TODO: update the map of the DTMPC
+        pass
+
+    def laser_scan_callback(self, scan_msg: LaserScan):
+        # TODO: update the map of the DTMPC
+        pass
 
     def vel_lim_callback(self, v_max_msg: VelocityCommand):
         """Sets the upper bound on velocity limits for the planner"""
         prop_v_lim = np.clip(np.array(v_max_msg.v_x, v_max_msg.v_y, v_max_msg.w_z), 0, 1)
         self.v_max = prop_v_lim * self.v_max_bound
         self.v_min = prop_v_lim * self.v_min_bound
+
+        self.dtmpc.set_input_bounds(self.v_min, self.v_max)
 
     @staticmethod
     def quat2yaw(quat):
@@ -166,33 +186,18 @@ class DynamicTubeMPCNode(ObeliskController):
         if not self.recieved_path:
             return
         
-        z, v = self.solve_dtmpc()
+        z, v = self.dtmpc.solve()
 
         # setting the message
         traj_msg = Trajectory()
         traj_msg.header.stamp = self.get_clock().now().to_msg()
         traj_msg.z = z.tolist()
         traj_msg.v = v.tolist()
-        traj_msg.t = self.ts.tolist()
+        traj_msg.t = (self.ts + self.get_clock().nanoseconds() / 1e-9).tolist()
         
         self.obk_publishers["pub_ctrl"].publish(traj_msg)
         assert is_in_bound(type(traj_msg), ObeliskControlMsg)
         return traj_msg
-
-    def solve_dtmpc(self):
-        # TODO: solve DTMPC
-        # Compute z_ref, v_ref from path
-        
-
-        # Linearize obstacle constraints
-
-        # Take a few steps on SQP
-        z = ...
-        v = ...
-        return z, v
-    
-    
-
 
 
 def main(args: Optional[List] = None) -> None:
